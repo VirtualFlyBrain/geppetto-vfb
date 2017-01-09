552c2281135d4e02f35f72073d8087d3943ef474
Rob Court <robbie.court@gmail.com>
2017-01-09 10:39:27 +0000

    removing ID unless subject of search

--- ComponentsInitialization.js
+++ ComponentsInitialization.js
@@ -289,9 +289,9 @@ define(function (require) {
                     url: "http://vfbdev.inf.ed.ac.uk/search/select?fl=short_form,label,synonym,id,type,has_narrow_synonym_annotation,has_broad_synonym_annotation&start=0&fq=ontology_name:(fbbt)&fq=is_obsolete:false&fq=shortform_autosuggest:VFB_*%20OR%20shortform_autosuggest:FBbt_*&rows=250&bq=is_defining_ontology:true%5E100.0%20label_s:%22%22%5E2%20synonym_s:%22%22%20in_subset_annotation:BRAINNAME%5E3%20short_form:FBbt_00003982%5E2&q=*$SEARCH_TERM$*%20OR%20$SEARCH_TERM$&defType=edismax&qf=label%20synonym%20label_autosuggest_ws%20label_autosuggest_e%20label_autosuggest%20synonym_autosuggest_ws%20synonym_autosuggest_e%20synonym_autosuggest%20shortform_autosuggest%20has_narrow_synonym_annotation%20has_broad_synonym_annotation&wt=json&indent=true",
                     crossDomain: true,
                     id: "short_form",
-                    label: {field: "label", formatting: "$VALUE$ [$ID$]"},
+                    label: {field: "label", formatting: "$VALUE$"},
                     explode_fields: [{field: "short_form", formatting: "$VALUE$ ($LABEL$)"}],
-                    explode_arrays: [{field: "synonym", formatting: "$VALUE$ ($LABEL$)[$ID$]"}],
+                    explode_arrays: [{field: "synonym", formatting: "$VALUE$ ($LABEL$)"}],
                     type: {
                         class: {
                             icon: "fa-file-text-o",
