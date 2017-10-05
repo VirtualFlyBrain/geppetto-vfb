f39f9e09fb98b3fa9e1baf87551c112211e17bc9
jrmartin <yeshuarmartinez@gmail.com>
2017-10-05 07:34:36 -0700

    switch stack viewer button to way it was

--- ComponentsInitialization.js
+++ ComponentsInitialization.js
@@ -1197,23 +1197,12 @@ define(function (require) {
                                 "tooltip": "Show term info"
                             },
                             "stackBtn": {
-                            	"condition": "window.StackViewer1!=undefined;",
-                                "false": {
-                                	"actions": [
-                                        "window.addStackWidget();"
-                                    ],
-                                    "icon": "gpt-showplane",
-                                    "label": "",
-                                    "tooltip": "Show stack viewer"
-                                },
-                                "true": {
-                                	"actions": [
-                                        "StackViewer1.destroy();"
-                                    ],
-                                    "icon": "gpt-hideplane",
-                                    "label": "",
-                                    "tooltip": "Hide stack viewer"
-                                }
+                                "actions": [
+                                    "window.addStackWidget();"
+                                ],
+                                "icon": "gpt-stack",
+                                "label": "",
+                                "tooltip": "Show stack viewer"
                             },
                             "meshBtn": {
                             	"condition": "Canvas1.getWireframe();",
