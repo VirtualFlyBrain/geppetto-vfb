a3f476d9b2fc2706a708aeb968339d50e9bfd016
Rob Court <robbie.court@gmail.com>
2018-02-21 19:01:45 +0000

    moving port redirect removal

--- dockerFiles/startup.sh
+++ dockerFiles/startup.sh
@@ -7,10 +7,13 @@ echo 'Start of log...' > ~/serviceability/logs/log.log
 # deploy Geppetto
 cd /opt/geppetto/org.geppetto/utilities/source_setup 
 python update_server.py
+grep -rnwl '/home/virgo' -e "VFB_EMBEDDER_URL" | xargs sed -i "s|VFB_EMBEDDER_URL|$VFB_EMBEDDER_URL|g" 
 # output log
 tail --sleep-interval=5 -F ~/serviceability/logs/log.log & 
 # set java memory maximum 
 sed 's/XX:MaxPermSize=512m/XX:MaxPermSize=$MAXSIZE/g' -i /home/virgo/bin/dmk.sh
 sed 's/Xmx512m/Xmx$MAXSIZE/' -i /home/virgo/bin/dmk.sh
+#Remove redirect in tomcat config
+sed 's\redirectPort="8443"\\g' -i /home/virgo/configuration/tomcat-server.xml
 # start virgo server
 /home/virgo/bin/startup.sh 
