3477884db8586b50a3d8cce679c8239d56f56740
Rob Court <robbie.court@gmail.com>
2018-02-21 01:05:57 +0000

    Update control-panel-tests.js

--- tests/casperjs/control-panel-tests.js
+++ tests/casperjs/control-panel-tests.js
@@ -40,15 +40,9 @@ casper.test.begin('VFB control panel tests', 7, function suite(test) {
         casper.then(function(){
             this.waitForText('VFB_00017894', function () {
                 this.echo("Element JFRC2_template appeared in popup");
-            }, null, 30000);
-            
-            this.waitForSelector('div[id=Popup1_VFB_00017894_metadata_el_1]', function () {
-                test.assertExists('div[id=Popup1_VFB_00017894_metadata_el_1]', 'Term info correctly populated  for JFRC2_template after load');
-            }, null, 30000);
-            
-            this.waitForSelector('button[id=VFB_00017894_zoom_buttonBar_btn]', function () {
-                test.assertExists('button[id=VFB_00017894_zoom_buttonBar_btn]', 'Term info button bar button created');
-            }, null, 30000);
+            	test.assertVisible('div[id=Popup1_VFB_00017894_metadata_el_1]', 'Term info correctly populated  for JFRC2_template after load');
+		test.assertExists('button[id=VFB_00017894_zoom_buttonBar_btn]', 'Term info button bar button created');
+	    }, null, 30000);
             
             
         });
