229f1776f14a165716adf349e6e330d33f79efb0
Dario Del Piano <dario@metacell.us>
2019-04-24 13:23:05 +0100

    Error boundaries implemented for VFB application, in case of error the user can either open a ticket on github or simply reload the page with a click

--- ComponentsInitialization.js
+++ ComponentsInitialization.js
@@ -10,13 +10,16 @@ jQuery(function () {
   var Redirect = require('react-router-dom').Redirect;
   var Router = require('react-router-dom').BrowserRouter;
   var VFBMain = require('./components/VFBMain').default;
+  var ErrorCatcher = require('./components/interface/ErrorCatcher').default;
 
   ReactDOM.render(
+    <ErrorCatcher>
     <Router basename={GEPPETTO_CONFIGURATION.contextPath}>
       <Switch>
         <Route path="/geppetto" component={VFBMain} />
         <Redirect from="/" to="/geppetto" />
       </Switch>
     </Router>
+    </ErrorCatcher>
     , document.getElementById('mainContainer'));
 });
