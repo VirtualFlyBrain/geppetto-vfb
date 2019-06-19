39016f671f1af7b6dbb06c115ba9dd3ff52a9d6e
Rob Court <rcourt@ed.ac.uk>
2019-06-19 17:48:53 +0100

    updating to match
    https://github.com/openworm/geppetto-application/blob/test-puppeteer/.eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,14 @@
 module.exports = {
   extends: [
     "./node_modules/@geppettoengine/geppetto-client/.eslintrc.js",
+    "plugin:jest/recommended"
   ],
+  plugins: ["jest"],
+  globals: {
+    page: true,
+    browser: true,
+    context: true,
+    jestPuppeteer: true,
+  },
+
 };
