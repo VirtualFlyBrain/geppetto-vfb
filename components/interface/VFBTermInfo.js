daad3f758c4b51cffc36981ea2e9964c828ea43a
Rob Court <rcourt@ed.ac.uk>
2019-07-23 19:23:23 +0100

    Revert "swapping order"
    This reverts commit e338379d3dbd2385c2bea7777cae80fa1c5c6b2b.

--- components/interface/VFBTermInfo.js
+++ components/interface/VFBTermInfo.js
@@ -684,7 +684,7 @@ export default class VFBTermInfoWidget extends React.Component {
         });
         items = items.replace(',,', ',').replace('i=,', 'i=');
         try {
-          items = items + '&' + 'id=' + this.refs.termInfoRef.state.termInfoId.replace('_meta','');
+          items = 'id=' + this.refs.termInfoRef.state.termInfoId.replace('_meta','') + '&' + items;
         } catch (ignore) { }
         if (items != "i=") {
           parent.history.pushState({}, title, parent.location.pathname + "?" + items);
