c00d093ed2cc5f70b57b191f55d59b147eca912a
Rob Court <rcourt@ed.ac.uk>
2019-07-24 18:52:40 +0100

    timing check

--- components/VFBMain.js
+++ components/VFBMain.js
@@ -1051,7 +1051,9 @@ export default class VFBMain extends React.Component {
         that.addVfbId(idArray);
         if (idsTermInfoSubstring.length > 0) {
           this.idForTermInfo = idsTermInfoSubstring;
-          this.termInfoReference.setTermInfo(this.idForTermInfo + "_meta", window[this.idForTermInfo].getName());
+          if (this.termInfoReference !== undefined) {
+            this.termInfoReference.setTermInfo(this.idForTermInfo + "_meta", window[this.idForTermInfo].getName());
+          }
         }
       });
     }
