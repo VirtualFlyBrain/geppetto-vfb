37d2e166dc2d7e7205c9943ca7dd78a86060ef32
Rob Court <rcourt@ed.ac.uk>
2019-07-30 20:07:37 +0100

    changing after build

--- dockerFiles/startup.sh
+++ dockerFiles/startup.sh
@@ -10,14 +10,15 @@ echo $VFB_R_SERVER
 grep -rls http://r.virtualflybrain.org/ocpu/library/vfbr/R/vfb_nblast $HOME/workspace/org.geppetto.frontend | xargs sed -i "s@http://r.virtualflybrain.org/ocpu/library/vfbr/R/vfb_nblast@$VFB_R_SERVER@g" 
 echo $SOLR_SERVER
 grep -rls https://solr.virtualflybrain.org/solr/ontology/select $HOME/workspace/org.geppetto.frontend | xargs sed -i "s@https://solr.virtualflybrain.org/solr/ontology/select@$SOLR_SERVER@g" 
-echo "Google Analytics code: ${googleAnalyticsSiteCode}"
-grep -rls UA-45841517-1 $HOME/workspace/org.geppetto.frontend | xargs sed -i "s@UA-45841517-1@${googleAnalyticsSiteCode}@g" 
 
 # Frontend final build
 cd $HOME/workspace/org.geppetto.frontend 
 /bin/echo -e "\e[96mMaven install org.geppetto.frontend\e[0m"
 mvn -Dhttps.protocols=TLSv1.2 -DcontextPath=org.geppetto.frontend -DuseSsl=false -DskipTests install
 rm -rf src
+
+echo "Google Analytics code: ${googleAnalyticsSiteCode}"
+grep -rls UA-45841517-1 $HOME/ | xargs sed -i "s@UA-45841517-1@${googleAnalyticsSiteCode}@g" 
 
 # Start a logfile
 mkdir -p $SERVER_HOME/serviceability/logs
