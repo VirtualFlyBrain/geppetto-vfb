11879e96a0b4d19491badcf067873818e115f48f
Rob Court <rcourt@ed.ac.uk>
2019-08-06 21:13:53 +0100

    removing unmount

--- components/VFBMain.js
+++ components/VFBMain.js
@@ -1092,7 +1092,7 @@ export default class VFBMain extends React.Component {
     window.console.stderr = console.error.bind(console);
     window.console.logs = [];
     console.log = function () {
-      if (Array.from(arguments).join("\n").indexOf('Pixi.js') < 0) {
+      if (Array.from(arguments).join("\n").indexOf('Pixi.js') < 0 && Array.from(arguments).join("\n") != 'unmount') {
         window.ga('vfb.send', 'event', 'log', Array.from(arguments).join("\n"));
         window.console.logs.push('+ ' + Array.from(arguments).join('\n'));
         window.console.stdlog.apply(console, arguments);
