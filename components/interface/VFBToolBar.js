b89a59ac99d0c4e3fb4e0eaa2d72671eed18fa39
Rob Court <rcourt@ed.ac.uk>
2019-08-06 15:37:58 +0100

    typo fix

--- components/interface/VFBToolBar.js
+++ components/interface/VFBToolBar.js
@@ -397,7 +397,9 @@ export default class VFBToolBar extends React.Component {
       ).replace(
         "$SCREEN$", window.innerWidth + ',' + window.innerHeight
       ).replace(
-        "$LOG$", window.console.logs.join('</span>%0A').replace('- ','<span style="color:orange">').replace('+ ','<span style="color:yellow">')
+        "$LOG$", window.console.logs.join('%0A').replace('#','%23')
+      ).replace(
+        "$COLOURLOG$", window.console.logs.join('</span>%0A').replace('- ','<span style="color:orange">').replace('+ ','<span style="color:yellow">')
       )
     );    
   }
