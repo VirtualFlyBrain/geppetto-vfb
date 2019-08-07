8f141eca90819e0c20414eaa80cb55bfc38b0689
Rob Court <rcourt@ed.ac.uk>
2019-08-07 06:02:12 +0100

    return encode

--- components/interface/VFBToolBar.js
+++ components/interface/VFBToolBar.js
@@ -401,7 +401,7 @@ export default class VFBToolBar extends React.Component {
       ).replace(
         /\$SCREEN\$/g, window.innerWidth + ',' + window.innerHeight
       ).replace(
-        /\$LOG\$/g, window.console.logs.join('\n').replace(
+        /\$LOG\$/g, window.console.logs.join('%0A').replace(
           /\&/g,escape('&')
         ).replace(
           /\#/g,escape('#')
