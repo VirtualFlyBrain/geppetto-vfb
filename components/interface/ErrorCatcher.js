d05a1c21be921d06852f9b7f64ced1a0f43324d8
Rob Court <rcourt@ed.ac.uk>
2019-08-08 16:10:16 +0100

    limiting to last 10 lines of log

--- components/interface/ErrorCatcher.js
+++ components/interface/ErrorCatcher.js
@@ -47,7 +47,7 @@ class ErrorCatcher extends React.Component {
     
     handleClose = () => {
       var customMessage = "Steps to reproduce the problem: \n\nPlease fill the below with the necessary steps to reproduce the problem\n\n\n\nError Information:\n\n"
-      var url = "https://github.com/VirtualFlyBrain/VFB2/issues/new?body=" + customMessage + this.state.error.message + "\n\n" + this.state.error.stack.replace("#",escape("#")) + "\n\n```diff\n" + window.console.logs.join('\n').replace("#",escape("#")) + "\n```\n";
+      var url = "https://github.com/VirtualFlyBrain/VFB2/issues/new?body=" + customMessage + this.state.error.message + "\n\n" + this.state.error.stack.replace("#",escape("#")) + "\n\n```diff\n" + window.console.logs.slice(-10).join('\n').replace("#",escape("#")) + "\n```\n";
       var win = window.open(encodeURI(url), '_blank');
       win.focus();
     };
