7fb71704bb70053d3d8ca9552117bdd43553d02f
Rob Court <rcourt@ed.ac.uk>
2019-08-16 21:43:41 +0100

    reverting to single id

--- components/VFBMain.js
+++ components/VFBMain.js
@@ -334,7 +334,7 @@ export default class VFBMain extends React.Component {
             var anchorElement = domObj.filter('a');
             // extract ID
             var templateID = anchorElement.attr('data-instancepath');
-            this.fetchVariableThenRun([templateID,path], this.resolve3D);
+            this.fetchVariableThenRun(templateID, this.resolve3D);
             return; // Don't load until the template has
           }
         }
