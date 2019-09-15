d489256be7b27344ce89e2dd56c18113e2d053bb
Rob Court <rcourt@ed.ac.uk>
2019-09-15 21:41:53 +0100

    just removing

--- components/VFBMain.js
+++ components/VFBMain.js
@@ -1165,12 +1165,6 @@ export default class VFBMain extends React.Component {
           resultsControlConfig={this.queryResultsControlConfig}
           datasourceConfig={this.queryBuilderDatasourceConfig} />
 
-        <div id="tutorialDiv">
-          { 
-            //this.tutorialRender 
-          }
-        </div>
-
         {this.htmlToolbarRender}
       </div>
     );
