a5043f6c57e2ed999c8389ae8d21fc0fe92dcce2
jrmartin <yeshuarmartinez@gmail.com>
2019-12-23 17:34:02 -0800

    fixing test

--- tests/jest/vfb/menu-component-tests.js
+++ tests/jest/vfb/menu-component-tests.js
@@ -33,15 +33,15 @@ describe('VFB Menu Component Tests', () => {
 		it('Term info component created after load', async () => {
 			await wait4selector(page, 'div#VFBTermInfo_el_1_component', { visible: true , timeout : 120000})
 		})
-		
-		it('Hide Quick Help Modal Window', async () => {
-			closeModalWindow(page);
-			await wait4selector(page, 'div#quick_help_modal', { hidden : true })
-		})
 
 		// Wait for this component to load on term info, means page has finished loading
 		it('Term info component correctly populated at startup', async () => {
 			await page.waitForFunction('document.getElementById("VFBTermInfo_el_0_component").innerText.startsWith("adult brain template JFRC2 (VFB_00017894)")', {timeout : 120000});
+		})
+		
+		it('Hide Quick Help Modal Window', async () => {
+			closeModalWindow(page);
+			await wait4selector(page, 'div#quick_help_modal', { hidden : true })
 		})
 	})
 
@@ -56,14 +56,9 @@ describe('VFB Menu Component Tests', () => {
 			expect(dropDownMenuItems).toEqual(4);
 		})
 
-		it('About Modal Appears', async () => {
-			await page.evaluate(async () => document.getElementById("About").click());
-			// Wait for selector to appear, this means About modal was opened
-			await wait4selector(page, '#vfb-content-block', { visible: true })
-		})
-
 		// Tests modal title bar is populated with expected title for About modal
 		it('About Modal Title Correct', async () => {
+			await page.evaluate(async () => document.getElementById("About").click());
 			await page.waitForFunction('document.getElementById("vfb-content-titlebar").innerText.startsWith("About Virtual Fly Brain")');
 		})
 
