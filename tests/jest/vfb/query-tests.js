5e3e4e6a797e34c69e6a94d7f32a746ee8fef8c1
Rob Court <rcourt@ed.ac.uk>
2020-01-17 10:12:58 +0000

    moved to function

--- tests/jest/vfb/query-tests.js
+++ tests/jest/vfb/query-tests.js
@@ -81,7 +81,9 @@ describe('VFB Query Component Tests', () => {
 		})
 
 		it('Checking enough results returned', async () => {
-			await page.waitForFunction('Number(document.getElementById("query-results-label").innerText.split(" ")[0])>3000', {timeout : 1000});
+			await page.waitForFunction(function(args){
+				return Number(document.getElementById("query-results-label").innerText.split(" ")[0])>Number(args);
+			  }, {timeout : 10000}, 3000);
 		})
 
 		it('Running query. Results rows appeared - click on results info for JFRC2 example of medulla', async () => {
