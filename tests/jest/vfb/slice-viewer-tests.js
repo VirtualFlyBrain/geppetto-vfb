2bcd7f44bcc6f9f90d319080e1e2b2c8cb14ff84
Rob Court <rcourt@ed.ac.uk>
2020-05-27 18:19:54 +0100

    no help menu open

--- tests/jest/vfb/slice-viewer-tests.js
+++ tests/jest/vfb/slice-viewer-tests.js
@@ -43,10 +43,10 @@ describe('VFB Slice Viewer Component Tests', () => {
 			await wait4selector(page, 'div#VFBTermInfo_el_1_component', { visible: true })
 		})
 		
-		it('Hide Quick Help Modal Window', async () => {
-			closeModalWindow(page);
-			await wait4selector(page, 'div#quick_help_modal', { hidden : true })
-		})
+//		it('Hide Quick Help Modal Window', async () => {
+//			closeModalWindow(page);
+//			await wait4selector(page, 'div#quick_help_modal', { hidden : true })
+//		})
 
 		it('Term info component correctly populated at startup', async () => {
 			await page.waitForFunction('document.getElementById("VFBTermInfo_el_0_component").innerText.startsWith("adult brain template JFRC2 (VFB_00017894)")');
