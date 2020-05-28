766815092ff49348db95ba3903f01e4a36a36edb
Rob Court <rcourt@ed.ac.uk>
2020-05-28 09:38:26 +0100

    allowing longer load time

--- tests/jest/vfb/tree-browser-tests.js
+++ tests/jest/vfb/tree-browser-tests.js
@@ -86,7 +86,7 @@ describe('VFB Tree Browser Component Tests', () => {
 		it('Click on "eye" icon to render "adult cerebral ganglion" mesh', async () => {
 			await click(page, '#VFBTree_component i.fa-eye');
 			// Wait for 'color picker' selector to show, this is the sign that the click on the eye button worked and the mesh was rendered
-			await wait4selector(page, 'i.fa-tint', { visible: true, timeout : 240000 })
+			await wait4selector(page, 'i.fa-tint', { visible: true, timeout : 500000 })
 		})
 
 		it('Mesh for "adult cerebral ganglion" rendered in canvas after clicking on eye icon next to node', async () => {
