5d7ec315b75b5f91b9b35b3aea1dc8bb4bf7f871
Rob Court <rcourt@ed.ac.uk>
2020-05-29 12:05:36 +0100

    increasing delays

--- tests/jest/vfb/term-info-tests.js
+++ tests/jest/vfb/term-info-tests.js
@@ -116,7 +116,7 @@ describe('VFB Term Info Component Tests', () => {
 		it('Term info, hide volume using "Hide 3D Volume"', async () => {
 			// Click on Term Info Drop Down Menu
 			await page.evaluate(async () => document.querySelectorAll(".focusTermRight button")[0].click());
-			await wait4selector(page, 'div#simple-popper', { visible: true, timeout : 5000});
+			await wait4selector(page, 'div#simple-popper', { visible: true, timeout : 50000});
 			await page.evaluate(async () => document.getElementById("Hide 3D Volume").click());
 			await page.waitFor(2000);
 			// Test Canvas Container doesn't have volume mesh visible anymore
@@ -128,7 +128,7 @@ describe('VFB Term Info Component Tests', () => {
 		it('Term info ,show volume using "Show 3D Volume"', async () => {
 			// Click on Term Info Drop Down Menu
 			await page.evaluate(async variableName => document.querySelectorAll(".focusTermRight button")[0].click());
-			await wait4selector(page, 'div#simple-popper', { visible: true, timeout : 5000});
+			await wait4selector(page, 'div#simple-popper', { visible: true, timeout : 50000});
 			// Click on Show 3D Volume menu option
 			await page.evaluate(async () => document.getElementById("Show 3D Volume").click());
 			await page.waitFor(2000);
@@ -144,16 +144,16 @@ describe('VFB Term Info Component Tests', () => {
 				let flexComponents = document.getElementsByClassName("flexlayout__tab_button_trailing").length;
 				document.getElementsByClassName("flexlayout__tab_button_trailing")[flexComponents-1].click();
 			});			
-			await wait4selector(page, '#vfbterminfowidget', { hidden: true, timeout : 5000})
+			await wait4selector(page, '#vfbterminfowidget', { hidden: true, timeout : 50000})
 		})
 
 		it('Term info , open using "Show Info" menu option', async () => {
 			// Click on Term Info Drop Down Menu
 			await page.evaluate(async variableName => document.querySelectorAll(".focusTermRight button")[0].click());
-			await wait4selector(page, 'div#simple-popper', { visible: true, timeout : 5000});
+			await wait4selector(page, 'div#simple-popper', { visible: true, timeout : 50000});
 			// Click on 'Show Info' menu selection option
 			await page.evaluate(async () => document.getElementById("Show Info").click());
-			await wait4selector(page, 'div#vfbterminfowidget', { visible: true, timeout : 5000});
+			await wait4selector(page, 'div#vfbterminfowidget', { visible: true, timeout : 50000});
 			await wait4selector(page, 'div#VFBTermInfo_el_1_component', { visible: true, timeout : 5000});
 		})
 
@@ -162,7 +162,7 @@ describe('VFB Term Info Component Tests', () => {
 			await page.waitFor(20000);
 			// Click on Term Info Drop Down Menu
 			await page.evaluate(async variableName => document.querySelectorAll(".focusTermRight button")[0].click());
-			await wait4selector(page, 'div#simple-popper', { visible: true, timeout : 5000});
+			await wait4selector(page, 'div#simple-popper', { visible: true, timeout : 50000});
 			// Mouse over 'Query For' menu item to expand drop down menu
 			await page.evaluate(async () => {
 				let hover = document.querySelectorAll("[id='Query for'] .fa-chevron-right")[0];
@@ -176,13 +176,13 @@ describe('VFB Term Info Component Tests', () => {
 			await page.waitFor(1000);
 			// Click on item from query drop down menu and expect the query modal window to open
 			await page.evaluate(async () => document.getElementById("List all example images of medulla").click());
-			await wait4selector(page, '#query-results-container', { visible: true, timeout : 5000});
+			await wait4selector(page, '#query-results-container', { visible: true, timeout : 50000});
 		})
 
 		// Close Query Results window by pressing Escape on Window
 		it('Close Query Results Window', async () => {
 			closeModalWindow(page);
-			await wait4selector(page, '#query-results-container', { hidden: true, timeout : 5000});
+			await wait4selector(page, '#query-results-container', { hidden: true, timeout : 50000});
 		})
 
 		it('Term info correctly populated after clicking on Source Link', async () => {
@@ -194,10 +194,10 @@ describe('VFB Term Info Component Tests', () => {
 	describe('Test Term Info Icon Buttons Work', () => {
 		it('Term info, "Spotlight" Button Works', async () => {
 			await page.evaluate(async variableName => $(variableName).click(), "i.fa-search");
-			await wait4selector(page, ST.SPOT_LIGHT_SELECTOR, {visible: true, timeout : 5000});
+			await wait4selector(page, ST.SPOT_LIGHT_SELECTOR, {visible: true, timeout : 50000});
 			// Close Spotlight
 			closeModalWindow(page);
-			await wait4selector(page, ST.SPOT_LIGHT_SELECTOR, { hidden: true, timeout : 5000});
+			await wait4selector(page, ST.SPOT_LIGHT_SELECTOR, { hidden: true, timeout : 50000});
 		})
 
 		it('Term info, "Control Panel" Button Works', async () => {
@@ -207,15 +207,15 @@ describe('VFB Term Info Component Tests', () => {
 			expect(rows).toEqual(4);
 			// Close Control Panel
 			closeModalWindow(page);
-			await wait4selector(page, ST.CONTROL_PANEL_SELECTOR, { hidden: true, timeout : 5000});
+			await wait4selector(page, ST.CONTROL_PANEL_SELECTOR, { hidden: true, timeout : 50000});
 		})
 
 		it('Term info, "Query Button" Works', async () => {
 			await page.evaluate(async variableName => $(variableName).click(), "i.fa-quora");
-			await wait4selector(page, '#query-results-container', { visible: true ,timeout : 5000 });
+			await wait4selector(page, '#query-results-container', { visible: true ,timeout : 50000 });
 			// Close Query Panel
 			closeModalWindow(page);
-			await wait4selector(page, '#query-results-container', { hidden: true, timeout : 5000});
+			await wait4selector(page, '#query-results-container', { hidden: true, timeout : 50000});
 		})
 
 		it('Term info, "Clear All" Button Works', async () => {
