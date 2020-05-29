bd8ae9759030954c8d3b1aa7c5ac8d036eac88f3
Rob Court <rcourt@ed.ac.uk>
2020-05-29 09:01:41 +0100

    fixes from Jesus with some tweaks

--- tests/jest/vfb/utils.js
+++ tests/jest/vfb/utils.js
@@ -38,19 +38,24 @@ export const closeModalWindow = async (page) => {
 }
 
 export const flexWindowClick = async (title, selector) => {
-  if (document.getElementsByClassName("flexlayout__tab_button_content")[0].innerText == title) {
-    await document.getElementsByClassName(selector)[0].click();
-  }else if (document.getElementsByClassName("flexlayout__tab_button_content")[1].innerText == title) {
-    await document.getElementsByClassName(selector)[1].click();
-  }else if (document.getElementsByClassName("flexlayout__tab_button_content")[2].innerText == title) {
-    await document.getElementsByClassName(selector)[2].click();
-  }else if (document.getElementsByClassName("flexlayout__tab_button_content")[3].innerText == title) {
-    await document.getElementsByClassName(selector)[3].click();
-  }else if (document.getElementsByClassName("flexlayout__tab_button_content")[4].innerText == title) {
-    await document.getElementsByClassName(selector)[4].click();
-  }else if (document.getElementsByClassName("flexlayout__tab_button_content")[5].innerText == title) {
-    await document.getElementsByClassName(selector)[5].click();
-  }else if (document.getElementsByClassName("flexlayout__tab_button_content")[6].innerText == title) {
-    await document.getElementsByClassName(selector)[6].click();
+  await page.evaluate((title, selector) => {
+    if (document.getElementsByClassName("flexlayout__tab_button_content")  != undefined && document.getElementsByClassName("flexlayout__tab_button_content").length != undefined && document.getElementsByClassName("flexlayout__tab_button_content").length > 0) {
+      if (document.getElementsByClassName("flexlayout__tab_button_content")[0].innerText == title) {
+        document.getElementsByClassName(selector)[0].click();
+      }else if (document.getElementsByClassName("flexlayout__tab_button_content").length > 1 && document.getElementsByClassName("flexlayout__tab_button_content")[1].innerText == title) {
+        document.getElementsByClassName(selector)[1].click();
+      }else if (document.getElementsByClassName("flexlayout__tab_button_content").length > 2 && document.getElementsByClassName("flexlayout__tab_button_content")[2].innerText == title) {
+        document.getElementsByClassName(selector)[2].click();
+      }else if (document.getElementsByClassName("flexlayout__tab_button_content").length > 3 && document.getElementsByClassName("flexlayout__tab_button_content")[3].innerText == title) {
+        document.getElementsByClassName(selector)[3].click();
+      }else if (document.getElementsByClassName("flexlayout__tab_button_content").length > 4 && document.getElementsByClassName("flexlayout__tab_button_content")[4].innerText == title) {
+        document.getElementsByClassName(selector)[4].click();
+      }else if (document.getElementsByClassName("flexlayout__tab_button_content").length > 5 && document.getElementsByClassName("flexlayout__tab_button_content")[5].innerText == title) {
+        document.getElementsByClassName(selector)[5].click();
+      }else if (document.getElementsByClassName("flexlayout__tab_button_content").length > 6 && document.getElementsByClassName("flexlayout__tab_button_content")[6].innerText == title) {
+        document.getElementsByClassName(selector)[6].click();
+      }
+    }
   }
+  , title, selector);
 }
