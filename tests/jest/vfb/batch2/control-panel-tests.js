04ff3f5c9dad002c142caeb4f3acd36c417ea430
Rob Court <rcourt@ed.ac.uk>
2020-06-02 11:38:26 +0100

    no quickhelp

--- tests/jest/vfb/batch2/control-panel-tests.js
+++ tests/jest/vfb/batch2/control-panel-tests.js
@@ -41,13 +41,13 @@ describe('VFB Control Panel Tests', () => {
 
 		it('Term info component created after load', async () => {
 			await wait4selector(page, 'div#VFBTermInfo_el_2_component', { visible: true })
-		})		
-
-		it('Hide Quick Help Modal Window', async () => {
-			closeModalWindow(page);
-			await wait4selector(page, 'div#quick_help_modal', { hidden : true })
 		})
-		
+
+//		it('Hide Quick Help Modal Window', async () => {
+//			closeModalWindow(page);
+//			await wait4selector(page, 'div#quick_help_modal', { hidden : true })
+//		})
+
 		it('Term info component correctly populated at startup', async () => {
 			await page.waitForFunction('document.getElementById("VFBTermInfo_el_0_component").innerText.startsWith("fru-M-200266 (VFB_00000001)")');
 		})
