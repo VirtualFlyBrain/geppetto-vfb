7c74bef70f30e95f759f4b468fd1757025ec28aa
Rob Court <rcourt@ed.ac.uk>
2020-06-13 10:36:40 +0100

    typo fix

--- components/interface/VFBStackViewer/VFBStackViewer.js
+++ components/interface/VFBStackViewer/VFBStackViewer.js
@@ -251,9 +251,9 @@ export default class VFBStackViewer extends React.Component {
         templateId: 'NOTSET'
       };
     } else if (this.config.subDomains != undefined && this.config.subDomains[0] != undefined && this.config.subDomains[0].length > 2) {
-      this.voxel.x = Number(this.config.subDomains[0][0] || 0.622088);
-      this.voxel.y = Number(this.config.subDomains[0][1] || 0.622088);
-      this.voxel.z = Number(this.config.subDomains[0][2] || 0.622088);
+      this.voxelSize.x = Number(this.config.subDomains[0][0] || 0.622088);
+      this.voxelSize.y = Number(this.config.subDomains[0][1] || 0.622088);
+      this.voxelSize.z = Number(this.config.subDomains[0][2] || 0.622088);
     }
 
     return (
