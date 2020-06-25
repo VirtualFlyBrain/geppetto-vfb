4a0933d090f0ad2dcf7f78e24f28b912f02f24ba
Rob Court <rcourt@ed.ac.uk>
2020-06-25 10:03:01 +0100

    removing + tab

--- components/interface/VFBFocusTerm/VFBFocusTerm.js
+++ components/interface/VFBFocusTerm/VFBFocusTerm.js
@@ -467,7 +467,6 @@ export default class VFBFocusTerm extends React.Component {
               <Tabs>
                 <TabList>
                   <Tab>{(window.templateID !== undefined) ? window[window.templateID].getName() : "Template"}</Tab>
-                  <Tab disabled={true}>&nbsp; + &nbsp;</Tab>
                 </TabList>
                 <TabPanel style={{ display: "none" }}/>
                 <TabPanel style={{ display: "none" }}/>
