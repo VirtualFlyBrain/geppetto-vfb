3810247bc8fa13a1ac2a90814096b852e5300b10
Rob Court <rcourt@ed.ac.uk>
2020-06-27 09:58:04 +0100

    only setting geometry on swc

--- components/configuration/VFBTermInfo/VFBTermInfoConfiguration.js
+++ components/configuration/VFBTermInfo/VFBTermInfoConfiguration.js
@@ -98,14 +98,14 @@ const buttonBarConfiguration = {
       "condition": "(function() { if ($instance$.getType().$instance$_swc != undefined && $instance$.geometryType === \"CYLINDERS\" ) { return true; } else { return false; } })()",
       "false": {
         "id": "cylynders_swc",
-        "actions": ["$instance$.setGeometryType('cylinders'); $instance$.geometryType = \"CYLINDERS\" "],
+        "actions": ["var instance = Instances.getInstance('$instance$.$instance$_swc'); instance.setGeometryType('cylinders'); $instance$.geometryType = \"CYLINDERS\" "],
         "icon": "fa-plus-circle",
         "label": "Cylinder",
         "tooltip": "Cylinder 3D Skeleton"
       },
       "true": {
         "id": "cylynders_swc",
-        "actions": ["$instance$.setGeometryType('lines'); $instance$.geometryType = \"LINES\" "],
+        "actions": ["var instance = Instances.getInstance('$instance$.$instance$_swc'); instance.setGeometryType('lines'); $instance$.geometryType = \"LINES\" "],
         "icon": "fa-minus-circle",
         "label": "Lines",
         "tooltip": "Lines 3D Skeleton"
