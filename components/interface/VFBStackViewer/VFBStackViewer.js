49eb2776cbf847d808235524a5cbac613866db26
Rob Court <rcourt@ed.ac.uk>
2020-10-06 09:20:30 +0100

    Revert "syntax fix"
    This reverts commit 29a7025f933397ecb28074ddf3d422f92cd35832.

--- components/interface/VFBStackViewer/VFBStackViewer.js
+++ components/interface/VFBStackViewer/VFBStackViewer.js
@@ -125,7 +125,6 @@ export default class VFBStackViewer extends React.Component {
     } else {
       added = instances;
       var c = 0;
-      var i;
       for (i in instances){
         if (i.parent && i.parent.getId() == window.templateID) {
           added[0] = i;
