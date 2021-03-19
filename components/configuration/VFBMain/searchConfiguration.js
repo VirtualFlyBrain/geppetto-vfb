2bf900f8f0d4d179ab41a73720c0be26c3652c37
Rob Court <rcourt@ed.ac.uk>
2021-03-19 20:35:32 +0000

    setting to match client settings

--- components/configuration/VFBMain/searchConfiguration.js
+++ components/configuration/VFBMain/searchConfiguration.js
@@ -173,38 +173,54 @@ var searchConfiguration = {
   ],
   "sorter": function (a, b) {
     var InputString = window.spotlightString;
-    // move down results with no label
-    if (a.label == undefined) {
-      return 1;
-    }
-    if (b.label == undefined) {
-      return -1;
-    }
-    var aLabel = a.label.split(' (');
-    var aEnd = aLabel.pop(aLabel.length);
-    aLabel = aLabel.join(' (');
-    var bLabel = b.label.split(' (');
-    var bEnd = bLabel.pop(bLabel.length);
-    bLabel = bLabel.join(' (');
     // move exact matches to top
-    if (InputString == aLabel) {
+    if (InputString == a.label) {
       return -1;
     }
-    if (InputString == bLabel) {
-      return 1;
+    if (InputString == b.label) {
+        return 1;
     }
     // close match without case matching
     if (InputString.toLowerCase() == a.label.toLowerCase()) {
-      return -1;
+        return -1;
     }
     if (InputString.toLowerCase() == b.label.toLowerCase()) {
-      return 1;
+        return 1;
     }
+    // match ignoring joinging nonwords
+    if (InputString.toLowerCase().split(/\W+/).join(' ') == a.label.toLowerCase().split(/\W+/).join(' ')) {
+        return -1;
+    }
+    if (InputString.toLowerCase().split(/\W+/).join(' ') == b.label.toLowerCase().split(/\W+/).join(' ')) {
+        return 1;
+    }
+    // match against id
+    if (InputString.toLowerCase() == a.id.toLowerCase()) {
+        return -1;
+    }
+    if (InputString.toLowerCase() == b.id.toLowerCase()) {
+        return 1;
+    }
+    // pick up any match without nonword join character match
+    if (a.label.toLowerCase().split(/\W+/).join(' ').indexOf(InputString.toLowerCase().split(/\W+/).join(' ')) < 0 && b.label.toLowerCase().split(/\W+/).join(' ').indexOf(InputString.toLowerCase().split(/\W+/).join(' ')) > -1) {
+        return 1;
+    }
+    if (b.label.toLowerCase().split(/\W+/).join(' ').indexOf(InputString.toLowerCase().split(/\W+/).join(' ')) < 0 && a.label.toLowerCase().split(/\W+/).join(' ').indexOf(InputString.toLowerCase().split(/\W+/).join(' ')) > -1) {
+        return -1;
+    }
+    // also with underscores ignored
+    if (a.label.toLowerCase().split(/\W+/).join(' ').replace('_', ' ').indexOf(InputString.toLowerCase().split(/\W+/).join(' ').replace('_', ' ')) < 0 && b.label.toLowerCase().split(/\W+/).join(' ').replace('_', ' ').indexOf(InputString.toLowerCase().split(/\W+/).join(' ').replace('_', ' ')) > -1) {
+        return 1;
+    }
+    if (b.label.toLowerCase().split(/\W+/).join(' ').replace('_', ' ').indexOf(InputString.toLowerCase().split(/\W+/).join(' ').replace('_', ' ')) < 0 && a.label.toLowerCase().split(/\W+/).join(' ').replace('_', ' ').indexOf(InputString.toLowerCase().split(/\W+/).join(' ').replace('_', ' ')) > -1) {
+        return -1;
+    }
+    // find all matching spaced words
     if (InputString.toLowerCase().indexOf(' ') > -1) {
-      var lcInputStingFac = InputString.toLowerCase().split(/\W+/).filter(Boolean);
+      var lcInputStingFac = InputString.toLowerCase().split(' ');
       var compare = (a1, a2) => a1.filter(v => a2.includes(v)).length;
-      var cA = compare(lcInputStingFac, a.label.toLowerCase().split(/\W+/).filter(Boolean));
-      var cB = compare(lcInputStingFac, b.label.toLowerCase().split(/\W+/).filter(Boolean));
+      var cA = compare(lcInputStingFac, a.label.toLowerCase().split(' '));
+      var cB = compare(lcInputStingFac, b.label.toLowerCase().split(' '));
       if (cA > 0 || cB > 0) {
         if (cA > cB) {
           return -1;
@@ -214,76 +230,70 @@ var searchConfiguration = {
         }
       }
     }
-    // close match without case matching ['xx ('ID/Label)]
-    if (a.label.toLowerCase().indexOf(InputString.toLowerCase()) == 0) {
-      return -1;
+    // find all tokenised word matches
+    if (InputString.split(/\W+/).length > 1) {
+      var lcInputStingFac = InputString.toLowerCase().split(/\W+/);
+      var compare = (a1, a2) => a1.filter(v => a2.includes(v)).length;
+      var cA = compare(lcInputStingFac, a.label.toLowerCase().split(/\W+/));
+      var cB = compare(lcInputStingFac, b.label.toLowerCase().split(/\W+/));
+      if (cA > 0 || cB > 0) {
+        if (cA > cB) {
+          return -1;
+        }
+        if (cA < cB) {
+          return 1;
+        }
+      }
     }
-    if (b.label.toLowerCase().indexOf(InputString.toLowerCase()) == 0) {
-      return 1;
-    }
-    // match ignoring joinging nonwords
-    if (InputString.toLowerCase().split(/\W+/).join(' ') == a.label.toLowerCase().split(/\W+/).join(' ')) {
-      return -1;
-    }
-    if (InputString.toLowerCase().split(/\W+/).join(' ') == b.label.toLowerCase().split(/\W+/).join(' ')) {
-      return 1;
-    }
-    // match against id
-    if (InputString.toLowerCase() == a.id.toLowerCase()) {
-      return -1;
-    }
-    if (InputString.toLowerCase() == b.id.toLowerCase()) {
-      return 1;
-    }
-    // pick up any match without nonword join character match
-    if (a.label.toLowerCase().split(/\W+/).join(' ').indexOf(InputString.toLowerCase().split(/\W+/).join(' ')) < 0 && b.label.toLowerCase().split(/\W+/).join(' ').indexOf(InputString.toLowerCase().split(/\W+/).join(' ')) > -1) {
-      return 1;
-    }
-    if (b.label.toLowerCase().split(/\W+/).join(' ').indexOf(InputString.toLowerCase().split(/\W+/).join(' ')) < 0 && a.label.toLowerCase().split(/\W+/).join(' ').indexOf(InputString.toLowerCase().split(/\W+/).join(' ')) > -1) {
-      return -1;
-    }
-    // also with underscores ignored
-    if (a.label.toLowerCase().split(/\W+/).join(' ').replace('_', ' ').indexOf(InputString.toLowerCase().split(/\W+/).join(' ').replace('_', ' ')) < 0 && b.label.toLowerCase().split(/\W+/).join(' ').replace('_', ' ').indexOf(InputString.toLowerCase().split(/\W+/).join(' ').replace('_', ' ')) > -1) {
-      return 1;
-    }
-    if (b.label.toLowerCase().split(/\W+/).join(' ').replace('_', ' ').indexOf(InputString.toLowerCase().split(/\W+/).join(' ').replace('_', ' ')) < 0 && a.label.toLowerCase().split(/\W+/).join(' ').replace('_', ' ').indexOf(InputString.toLowerCase().split(/\W+/).join(' ').replace('_', ' ')) > -1) {
-      return -1;
-    }
-    // pick up any match without non alpha numeric join character match
-    if (a.label.toLowerCase().split(/\W+/).join(' ').replace(/[\W_]+/g,' ').indexOf(InputString.toLowerCase().split(/\W+/).join(' ').replace(/[\W_]+/g,' ')) < 0 && b.label.toLowerCase().split(/\W+/).join(' ').replace(/[\W_]+/g,' ').indexOf(InputString.toLowerCase().split(/\W+/).join(' ').replace(/[\W_]+/g,' ')) > -1) {
-      return 1;
-    }
-    if (b.label.toLowerCase().split(/\W+/).join(' ').replace(/[\W_]+/g,' ').indexOf(InputString.toLowerCase().split(/\W+/).join(' ').replace(/[\W_]+/g,' ')) < 0 && a.label.toLowerCase().split(/\W+/).join(' ').replace(/[\W_]+/g,' ').indexOf(InputString.toLowerCase().split(/\W+/).join(' ').replace(/[\W_]+/g,' ')) > -1) {
-      return -1;
+    // prioritise matches in the primary label
+    if (InputString.split(/\W+/).length > 1) {
+      var lcInputStingFac = InputString.toLowerCase().split(/\W+/);
+      var compare = (a1, a2) => a1.filter(v => a2.includes(v)).length;
+      var aLabel = a.label.split(' (');
+      var aEnd = aLabel.pop(aLabel.length);
+      aLabel = aLabel.join(' (');
+      var bLabel = b.label.split(' (');
+      var bEnd = bLabel.pop(bLabel.length);
+      bLabel = bLabel.join(' (');
+      var cA = compare(lcInputStingFac, aLabel.toLowerCase().split(/\W+/));
+      var cB = compare(lcInputStingFac, bLabel.toLowerCase().split(/\W+/));
+      if (cA > 0 || cB > 0) {
+        if (cA > cB) {
+          return -1;
+        }
+        if (cA < cB) {
+          return 1;
+        }
+      }
     }
     // if not found in one then advance the other
     if (a.label.toLowerCase().indexOf(InputString.toLowerCase()) < 0 && b.label.toLowerCase().indexOf(InputString.toLowerCase()) > -1) {
-      return 1;
+        return 1;
     }
     if (b.label.toLowerCase().indexOf(InputString.toLowerCase()) < 0 && a.label.toLowerCase().indexOf(InputString.toLowerCase()) > -1) {
-      return -1;
+        return -1;
     }
     // if the match is closer to start than the other move up
     if (a.label.toLowerCase().indexOf(InputString.toLowerCase()) > -1 && a.label.toLowerCase().indexOf(InputString.toLowerCase()) < b.label.toLowerCase().indexOf(InputString.toLowerCase())) {
-      return -1;
+        return -1;
     }
     if (b.label.toLowerCase().indexOf(InputString.toLowerCase()) > -1 && b.label.toLowerCase().indexOf(InputString.toLowerCase()) < a.label.toLowerCase().indexOf(InputString.toLowerCase())) {
-      return 1;
+        return 1;
     }
     // if the match in the id is closer to start then move up
     if (a.id.toLowerCase().indexOf(InputString.toLowerCase()) > -1 && a.id.toLowerCase().indexOf(InputString.toLowerCase()) < b.id.toLowerCase().indexOf(InputString.toLowerCase())) {
-      return -1;
+        return -1;
     }
     if (b.id.toLowerCase().indexOf(InputString.toLowerCase()) > -1 && b.id.toLowerCase().indexOf(InputString.toLowerCase()) < a.id.toLowerCase().indexOf(InputString.toLowerCase())) {
-      return 1;
+        return 1;
     }
     // move the shorter synonyms to the top
     if (a.label < b.label) {
-      return -1;
+        return -1;
     } else if (a.label > b.label) {
-      return 1;
+        return 1;
     } else {
-      return 0;
+        return 0;
     } // if nothing found then do nothing.
   },
   "clickHandler": function (id) {
