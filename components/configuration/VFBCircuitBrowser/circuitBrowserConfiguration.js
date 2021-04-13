f9bd78c2135a105ddc8d65a38c6f5d3e5b0bfe41
Rob Court <rcourt@ed.ac.uk>
2021-04-13 21:50:53 +0100

    typo fix

--- components/configuration/VFBCircuitBrowser/circuitBrowserConfiguration.js
+++ components/configuration/VFBCircuitBrowser/circuitBrowserConfiguration.js
@@ -17,7 +17,7 @@ var locationCypherQuery = ( instances, hops, weight ) => ({
       + "})"
       + " YIELD index, sourceNode, targetNode, nodeIds, path"
       + " OPTIONAL MATCH p=(source)-[r:synapsed_to*..]->(target) WHERE ALL(n in nodes(p) WHERE id(n) IN nodeIds)"
-      + " UNWIND r as sr WITH *, collect(id(sr)) as id OPTIONAL MATCH cp=(source)-[r:synapsed_to*..]-(target)"
+      + " UNWIND r as sr WITH *, collect(id(sr)) as ids OPTIONAL MATCH cp=(source)-[r:synapsed_to*..]-(target)"
       + " WHERE ALL(n in nodes(cp) WHERE id(n) IN nodeIds) UNWIND ids as id"
       + " RETURN distinct sourceNode as root, collect(cp), collect(distinct id) as fr ",
       "resultDataContents": ["row", "graph"]
