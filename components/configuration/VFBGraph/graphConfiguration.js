fcbd28146c49419f30ff00fd1345d58290fde08f
Rob Court <rcourt@ed.ac.uk>
2021-04-16 08:54:21 +0100

    old syntax fix

--- components/configuration/VFBGraph/graphConfiguration.js
+++ components/configuration/VFBGraph/graphConfiguration.js
@@ -12,7 +12,7 @@ var locationCypherQuery = instance => ({
 var whatCypherQuery = instance => ({
   "statements": [
     {
-      "statement": "MATCH (n:Entity {short_form:'" + instance + "'}) OPTIONAL MATCH p=(n)-[:INSTANCEOF|:SUBCLASSOF*..]->(x) "
+      "statement": "MATCH (n:Entity {short_form:'" + instance + "'}) OPTIONAL MATCH p=(n)-[:INSTANCEOF|SUBCLASSOF*..]->(x) "
       + "WHERE ('Anatomy' IN  labels(x)) OR (('Cell' IN  labels(x)) OR ('synaptic neuropil' IN labels(x))) "
       + " OR (('Ganglion' IN  labels(x)) OR ('Neuron_projection_bundle' IN labels(x))) "
       + "RETURN  n,p, n.short_form as root",
