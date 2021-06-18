6c3c4d5c414cf1830c4d9ba0790e5f40e8eb8316
jrmartin <yeshuarmartinez@gmail.com>
2021-06-18 09:37:34 -0700

    #1140 Fix hide/show controls behavior in layers component to match behavior in term info's button bar controls.

--- components/interface/VFBListViewer/ListViewerControlsMenu.js
+++ components/interface/VFBListViewer/ListViewerControlsMenu.js
@@ -47,9 +47,11 @@ class ListViewerControlsMenu extends Component {
     switch (action.handlerAction){
     case ACTIONS.SHOW:
       this.props.instance.show();
+      StackViewer1?.updateStackWidget();
       break;
     case ACTIONS.HIDE:
       this.props.instance.hide();
+      StackViewer1?.updateStackWidget();
       break;
     case ACTIONS.SELECT:
       this.props.instance.select();
