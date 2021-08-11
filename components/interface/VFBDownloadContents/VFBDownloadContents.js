6843948457c985f7b3e7680c63df6e137b9b31ae
jrmartin <yeshuarmartinez@gmail.com>
2021-08-11 11:48:25 -0700

    #1173 Downloader instances list

--- components/interface/VFBDownloadContents/VFBDownloadContents.js
+++ components/interface/VFBDownloadContents/VFBDownloadContents.js
@@ -8,6 +8,7 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import FormControl from '@material-ui/core/FormControl';
+import Typography from '@material-ui/core/Typography';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -18,91 +19,114 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Checkbox from '@material-ui/core/Checkbox';
 import Avatar from '@material-ui/core/Avatar';
 import { withStyles } from '@material-ui/styles';
 import axios from 'axios';
+import TreeView from "@material-ui/lab/TreeView";
+import TreeItem from "@material-ui/lab/TreeItem";
 
 const styles = theme => ({ listItemText: { fontSize:'1em' } });
+const ALL_INSTANCES = { id : "ALL_INSTANCES", name : "All Instances" };
+const NO_FILES_FOUND = "No files found";
 
+/**
+ * Component to download files contents 
+ */
 class VFBDownloadContents extends React.Component {
   constructor (props) {
     super(props);
 
     this.state = {
       open: false,
-      checked : [],
-      downloading : false
+      typesChecked : [],
+      downloadEnabled : false,
+      downloading : false,
+      downloadError : false,
+      instances : [ALL_INSTANCES],
+      selectedVariables : [],
+      allVariablesSelectedFlag : true
     }
 
     this.configuration = require('../../configuration/VFBDownloadContents/configuration');
     this.configurationOptions = this.configuration.options;
-    this.handleClickOpen = this.handleClickOpen.bind(this);
-    this.handleClose = this.handleClose.bind(this);
+    this.handleCloseDialog = this.handleCloseDialog.bind(this);
     this.openDialog = this.openDialog.bind(this);
-    this.handleToggle = this.handleToggle.bind(this);
+    this.handleTypeSelection = this.handleTypeSelection.bind(this);
     this.handleDownload = this.handleDownload.bind(this);
     this.extractVariableFileMeta = this.extractVariableFileMeta.bind(this);
     this.getAllLoadedVariables = this.getAllLoadedVariables.bind(this);
     this.requestZipDownload = this.requestZipDownload.bind(this);
-  }
-  
-  handleClickOpen () {
-    this.setState({ open : true });
+    this.getVariableById = this.getVariableById.bind(this);
+    this.toggleVariable = this.toggleVariable.bind(this);
+    this.dowloadErrorMessage = NO_FILES_FOUND;
   }
 
-  handleClose () {
+  handleCloseDialog () {
     this.setState({ open : false });
   }
   
   openDialog () {
-    this.setState({ open : true });
+    const variables = this.getAllLoadedVariables();
+    this.setState({ instances : variables , selectedVariables : variables, open : true });
   }
  
   handleDownload () {
-    let variables = this.getAllLoadedVariables();
     let json = { "entries" : [] };
     
-    variables.map( variable => {
+    this.state.selectedVariables.map( variable => {
       const filemeta = this.extractVariableFileMeta(variable);
       json.entries = json.entries.concat(filemeta);
     });
         
-    json.entries.length > 0 && this.requestZipDownload(json);
+    json.entries.length > 0 ? this.requestZipDownload(json) : this.setState( { downloadError : true });
   }
   
+  /**
+   * Extract filemeta from geppetto model, using variable id to find it
+   */
   extractVariableFileMeta (variable) {
-    const filemeta = window.Instances[variable][variable + "_meta"].variable.types[0].filemeta;
-    let filemetaText = filemeta.values[0].value.text;
+    let filemetaText = variable.filemeta.values[0].value.text;
     filemetaText = filemetaText.replace(/'/g, '"');
 
     const filemetaObject = JSON.parse(filemetaText);
     let filesArray = [];
     
-    this.state.checked.map( check => {
+    this.state.typesChecked.map( check => {
       filemetaObject[check] && filesArray.push({ "Url" : filemetaObject[check]?.url , "ZipPath" : filemetaObject[check]?.local })
     })
     
     return filesArray;
   }
  
+  /**
+   * Get array of all loaded variables in application
+   */
   getAllLoadedVariables () {
     let entities = GEPPETTO.ModelFactory.allPaths;
     var visuals = [];
 
     for (var i = 0; i < entities.length; i++) {
-      if (entities[i].metaType === "VisualType" || entities[i].metaType === "CompositeVisualType" ) {
-        visuals.push(entities[i].path.split(".")[0]);
+      if ( entities[i].metaType === "VisualType" || entities[i].metaType === "CompositeVisualType" ) {
+        const variable = entities[i].path.split(".")[0];
+        const instance = window.Instances[variable];
+        const filemeta = instance[variable + "_meta"].variable.types[0].filemeta;
+        visuals.push({ id : variable , name : instance.name , filemeta : filemeta });
       }
     }
 
     return visuals;
   }
   
+  /**
+   * Make axios call to download the zip
+   */
   requestZipDownload (jsonRequest) {
     let self = this;
 
-    this.setState( { downloading : true } );
+    this.setState( { downloadEnabled : false, downloading : true } );
     // Axios HTTP Post request with post query
     axios({
       method: 'post',
@@ -117,23 +141,60 @@ class VFBDownloadContents extends React.Component {
       link.setAttribute('download', self.configuration.zipName);
       document.body.appendChild(link);
       link.click();
-      setTimeout( () => self.setState( { downloading : false , open : false } ), 1000);
+      setTimeout( () => self.setState( { downloadEnabled : true, downloading : false, open : false } ), 500);
     }).catch( function (error) {
-      self.setState( { downloading : false } );
+      self.downloadErrorMessage = error?.message;
+      self.setState( { downloadEnabled : true, dowloadError : true, downloading : false } );
     })
   }
   
-  handleToggle (value) {
-    const currentIndex = this.state.checked.indexOf(value);
-    const newChecked = [...this.state.checked];
+  /**
+   * Handle checkbox selection of different types to download
+   */
+  handleTypeSelection (value) {
+    const currentIndex = this.state.typesChecked.indexOf(value);
+    const newTypesChecked = [...this.state.typesChecked];
 
     if (currentIndex === -1) {
-      newChecked.push(value);
+      newTypesChecked.push(value);
     } else {
-      newChecked.splice(currentIndex, 1);
+      newTypesChecked.splice(currentIndex, 1);
     }
 
-    this.setState({ checked : newChecked });
+    this.setState({ typesChecked : newTypesChecked, downloadEnabled : newTypesChecked.length > 0 , downloadError : false });
+  }
+  
+  /**
+   * Get variable by id, trigger by checkbox selection of variables
+   */
+  getVariableById (nodes, id) {
+    let variablesMatched = [];
+    
+    if ( id === ALL_INSTANCES.id ){
+      variablesMatched = nodes;
+    } else {
+      nodes.forEach(node => {
+        if ( node.id === id) {
+          variablesMatched.push(node);
+        }
+      });
+    }
+    
+    return variablesMatched;
+  }
+
+  /**
+   * Toggle variable selection from checklist
+   */
+  toggleVariable (checked, node) {
+    const allNode = this.getVariableById(this.state.instances, node.id);
+    let array = checked
+      ? [...this.state.selectedVariables, ...allNode]
+      : this.state.selectedVariables.filter(value => !allNode.includes(value));
+
+    array = array.filter((v, i) => array.indexOf(v) === i);
+
+    this.setState({ selectedVariables : array, allVariablesSelectedFlag : array.length > 0 , downloadError : false });
   }
 
   render () {
@@ -143,13 +204,13 @@ class VFBDownloadContents extends React.Component {
         fullWidth={this.state.fullWidth}
         maxWidth={this.state.maxWidth}
         open={this.state.open}
-        onClose={this.handleClose}
+        onClose={this.handleCloseDialog}
         aria-labelledby="max-width-dialog-title"
       >
-        <DialogTitle id="max-width-dialog-title"><h3>Download Contents</h3></DialogTitle>
+        <DialogTitle id="max-width-dialog-title" onClose={this.handleCloseDialog} ><h3>Download Contents</h3></DialogTitle>
         <DialogContent>
           <DialogContentText variant="h5">
-             Select all files to download as part of zip.
+             Select types
           </DialogContentText>
           <form noValidate>
             <FormControl >
@@ -158,30 +219,81 @@ class VFBDownloadContents extends React.Component {
                   const label = this.configurationOptions[value];
                   const labelId = `checkbox-list-secondary-label-${label}`;
                   return ( <ListItem key={value}>
-                    <ListItemText classes={{ primary:self.props.classes.listItemText }} id={labelId} primary={`${label}`} />
-                    <ListItemSecondaryAction>
-                      <Checkbox
-                        edge="end"
-                        onChange={() => self.handleToggle(value)}
-                        checked={this.state.checked.indexOf(value) !== -1}
-                        inputProps={{ 'aria-labelledby': labelId }}
-                        disabled={this.state.downloading}
-                      />
-                    </ListItemSecondaryAction>
+                    <Checkbox
+                      edge="start"
+                      onChange={() => self.handleTypeSelection(value)}
+                      checked={this.state.typesChecked.indexOf(value) !== -1}
+                      inputProps={{ 'aria-labelledby': labelId }}
+                      disabled={this.state.downloading}
+                    />
+                    <ListItemText variant="h5" classes={{ primary:self.props.classes.listItemText }} id={labelId} primary={`${label}`} />
                   </ListItem> );
                 })}
               </List>
             </FormControl>
           </form>
+          <DialogContentText variant="h5">
+            Select Variables:
+          </DialogContentText>
+          <TreeView
+            defaultCollapseIcon={<ExpandMoreIcon />}
+            defaultExpanded={["0"]}
+            defaultExpandIcon={<ChevronRightIcon />}
+          >
+            <TreeItem
+              key={ALL_INSTANCES.id}
+              nodeId={ALL_INSTANCES.id}
+              label={
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      onClick={e => e.stopPropagation()}
+                      checked={self.state.allVariablesSelectedFlag}
+                      onChange={event =>
+                        self.toggleVariable(event.currentTarget.checked, ALL_INSTANCES)
+                      }
+                    />
+                  }
+                  label={<Typography variant="h5" color="textPrimary">{ALL_INSTANCES.name}</Typography>}
+                  key={ALL_INSTANCES.id}
+                />
+              }
+            >
+              { this.state.instances.map(node => ( 
+                <TreeItem
+                  key={node.id}
+                  nodeId={node.id}
+                  label={
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          onClick={e => e.stopPropagation()}
+                          checked={self.state.selectedVariables.some(item => item.id === node.id)}
+                          onChange={event =>
+                            self.toggleVariable(event.currentTarget.checked, node)
+                          }
+                        />
+                      }
+                      label={<Typography variant="h5" color="textPrimary">{node.name}</Typography>}
+                      key={node.id}
+                    />
+                  }
+                />))
+              }
+            </TreeItem>
+          </TreeView>
         </DialogContent>
         <DialogActions>
           {
-            this.state.downloading ? <CircularProgress /> : null
+            this.state.downloading ? <CircularProgress align="left" size={24} /> : null
           }
-          <Button disabled={this.state.downloading} onClick={this.handleDownload} variant="contained" color="primary">
+          {
+            this.state.downloadError ? <Typography color="error">{ self.dowloadErrorMessage }</Typography> : null
+          }
+          <Button disabled={!this.state.downloadEnabled} onClick={this.handleDownload} variant="contained" color="primary">
             Download
           </Button>
-          <Button disabled={this.state.downloading} onClick={this.handleClose} color="primary">
+          <Button disabled={!this.state.downloadEnabled} onClick={this.handleCloseDialog} color="primary">
             Cancel
           </Button>
         </DialogActions>
