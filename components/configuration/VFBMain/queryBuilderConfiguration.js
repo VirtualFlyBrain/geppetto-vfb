e69a267eaf1773fe4f083fc28338251594eef0d9
Rob Court <rcourt@ed.ac.uk>
2022-11-29 11:35:46 +0000

    enabling new columns

--- components/configuration/VFBMain/queryBuilderConfiguration.js
+++ components/configuration/VFBMain/queryBuilderConfiguration.js
@@ -306,7 +306,7 @@ var queryResultsColMeta = [
 ];
 
 // which columns to display in the results
-var queryResultsColumns = ['name', 'neuron_A', 'type', 'downstream', 'tbars', 'upstream', 'weight', 'neuron_B', 'region', 'target', 'parent', 'expressed_in', 'description', 'reference', 'gross_type', 'stage', 'license', 'template', 'technique', 'controls', 'images', 'score', 'image_count'];
+var queryResultsColumns = ['name', 'cluster', 'neuron_A', 'type', 'cell_type', 'downstream', 'tbars', 'upstream', 'weight', 'neuron_B', 'region', 'target', 'parent', 'expressed_in', 'dataset', 'description', 'reference', 'gross_type', 'stage', 'license', 'template', 'technique', 'controls', 'images', 'score', 'image_count'];
 
 var queryResultsControlConfig = {
   "Common": {
