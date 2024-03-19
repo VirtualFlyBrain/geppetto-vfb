59475bb08bba9c0313ef03fac0f840bf61c6363e
Rob Court <rcourt@ed.ac.uk>
2024-03-19 18:36:10 +0000

    fixing children

--- .github/scripts/extractqueries.py
+++ .github/scripts/extractqueries.py
@@ -42,20 +42,19 @@ def process_queries(element, indent, ds_id, ds_name, ds_url, queries_info, names
     for query in element.findall('.//queries', namespaces=namespaces):
         query_id = query.get('id')
         query_name = query.get('name')
-        query_description = query.get('description', '')
+        query_description = query.get('description', 'No description provided')
         query_type = query.get('{http://www.w3.org/2001/XMLSchema-instance}type')
-        query_processor_id = query.get('queryProcessorId', 'Not provided')
-        query_string_encoded = query.get('query', '')
-        query_string_decoded = html.unescape(query_string_encoded)
-
-        query_content = "Processor ID: " + query_processor_id if query_type == "gep_2:ProcessQuery" else query_string_decoded
+        
+        # Adjust how query content is determined based on query type
+        query_content = "See below for child query details" if query_type == "gep_2:CompoundQuery" else query.get('query', 'No query provided')
+        query_content = html.unescape(query_content)
 
         query_entry = {
             'indent': indent,
-            'dataSourceID': ds_id,
-            'dataSourceName': ds_name,
-            'dataSourceURL': ds_url,
-            'queryID': query_id,
+            'dataSourceID': ds_id or "Not provided",
+            'dataSourceName': ds_name or "Not provided",
+            'dataSourceURL': ds_url or "Not provided",
+            'queryID': query_id or "Not provided",
             'queryName': query_name,
             'queryDescription': query_description,
             'queryType': query_type,
@@ -63,26 +62,29 @@ def process_queries(element, indent, ds_id, ds_name, ds_url, queries_info, names
             'childQueries': []
         }
 
-        queries_info.append(query_entry)
-
         if query_type == "gep_2:CompoundQuery":
-            process_child_queries(query, indent, ds_id, ds_name, ds_url, namespaces, query_entry)
+            process_child_queries(query, indent + "    ", ds_id, ds_name, ds_url, namespaces, query_entry)
+        
+        queries_info.append(query_entry)
 
 def generate_markdown_for_all_queries(queries_info):
     markdown_content = "# Queries and Chains Across Data Sources\n\n"
     for info in queries_info:
-        markdown_content += f"{info['indent']}## {info['queryName']}\n"
-        markdown_content += f"{info['indent']}*DataSource*: {info['dataSourceName']} (ID: {info['dataSourceID']})\n"
-        markdown_content += f"{info['indent']}*Description*: {info['queryDescription']}\n"
-        markdown_content += f"{info['indent']}*Query Type*: {info['queryType']}\n"
-        markdown_content += f"{info['indent']}*Query*: ```\n{info['query']}\n```\n\n"
-
-        for child_query in info['childQueries']:
-            markdown_content += f"{child_query['indent']}### {child_query['queryName']}\n"
-            markdown_content += f"{child_query['indent']}*Description*: {child_query['queryDescription']}\n"
-            markdown_content += f"{child_query['indent']}*Query*: ```\n{child_query['query']}\n```\n\n"
+        markdown_content += generate_markdown_for_query(info)
 
     return markdown_content
+
+def generate_markdown_for_query(query_info):
+    markdown = f"{query_info['indent']}## {query_info['queryName']}\n"
+    markdown += f"{query_info['indent']}*DataSource*: {query_info['dataSourceName']} (ID: {query_info['dataSourceID']})\n"
+    markdown += f"{query_info['indent']}*Description*: {query_info['queryDescription']}\n"
+    markdown += f"{query_info['indent']}*Query Type*: {query_info['queryType']}\n"
+    markdown += f"{query_info['indent']}*Query*: ```\n{query_info['query']}\n```\n\n"
+    
+    for child_query in query_info.get('childQueries', []):
+        markdown += generate_markdown_for_query(child_query)
+    
+    return markdown
 
 def save_to_file(content, file_path):
     with open(file_path, 'w', encoding='utf-8') as file:
