5fc9ec642e57cbe0555b488d74719f14a672927e
Rob Court <rcourt@ed.ac.uk>
2024-03-19 15:57:39 +0000

    Update extractqueries.py

--- .github/scripts/extractqueries.py
+++ .github/scripts/extractqueries.py
@@ -1,6 +1,6 @@
+import sys
 import html
 from lxml import etree
-import sys
 
 def parse_xmi(file_path):
     with open(file_path, 'rb') as file:
@@ -16,43 +16,62 @@ def extract_queries_and_chains(root, namespaces):
         ds_name = data_source.get('name')
         ds_url = data_source.get('url', 'No URL provided')
         
-        # Handling simple queries directly under dataSources
-        for query in data_source.findall('.//queries', namespaces=namespaces):
-            process_query(query, ds_id, ds_name, ds_url, queries_info, namespaces)
-        
-        # Handling compound queries with queryChain
-        for compound_query in data_source.findall('.//queryChain', namespaces=namespaces):
-            process_query(compound_query, ds_id, ds_name, ds_url, queries_info, namespaces)
+        process_queries(data_source, "", ds_id, ds_name, ds_url, queries_info, namespaces)
 
     return queries_info
 
-def process_query(query, ds_id, ds_name, ds_url, queries_info, namespaces):
-    query_id = query.get('id')
-    query_name = query.get('name')
-    query_description = query.get('description', '')
-    query_type = query.get('{http://www.w3.org/2001/XMLSchema-instance}type')
-    query_string_encoded = query.get('query', '')  # Assuming simple queries also use 'query' attribute
-    query_string_decoded = html.unescape(query_string_encoded)
-    
-    queries_info.append({
-        'dataSourceID': ds_id,
-        'dataSourceName': ds_name,
-        'dataSourceURL': ds_url,
-        'queryID': query_id,
-        'queryName': query_name,
-        'queryDescription': query_description,
-        'queryType': query_type,
-        'query': query_string_decoded
-    })
+def process_queries(element, indent, ds_id, ds_name, ds_url, queries_info, namespaces, parent_query_type=None):
+    for query in element.findall('.//queries | .//fetchVariableQuery', namespaces=namespaces):
+        query_id = query.get('id')
+        query_name = query.get('name')
+        query_description = query.get('description', '')
+        query_type = query.get('{http://www.w3.org/2001/XMLSchema-instance}type')
+        query_processor_id = query.get('queryProcessorId', 'Not provided')
+        query_string_encoded = query.get('query', '')  # Assume simple queries use 'query' attribute
+        query_string_decoded = html.unescape(query_string_encoded)
+
+        # Handle ProcessQuery type by using queryProcessorId
+        if query_type == "gep_2:ProcessQuery":
+            query_content = query_processor_id
+        else:
+            query_content = query_string_decoded
+
+        # For CompoundQuery types, indent child queries
+        if query_type == "gep_2:CompoundQuery":
+            queries_info.append({
+                'indent': indent,
+                'dataSourceID': ds_id,
+                'dataSourceName': ds_name,
+                'dataSourceURL': ds_url,
+                'queryID': query_id,
+                'queryName': query_name,
+                'queryDescription': query_description,
+                'queryType': query_type,
+                'query': 'Compound Query - See child steps'
+            })
+            # Recursively process child queries of a compound query
+            process_queries(query, indent + "    ", ds_id, ds_name, ds_url, queries_info, namespaces, query_type)
+        else:
+            queries_info.append({
+                'indent': indent,
+                'dataSourceID': ds_id,
+                'dataSourceName': ds_name,
+                'dataSourceURL': ds_url,
+                'queryID': query_id,
+                'queryName': query_name,
+                'queryDescription': query_description,
+                'queryType': query_type,
+                'query': query_content
+            })
 
 def generate_markdown_for_all_queries(queries_info):
     markdown_content = "# Queries and Chains Across Data Sources\n\n"
     for info in queries_info:
-        markdown_content += f"## DataSource: {info['dataSourceName']} (ID: {info['dataSourceID']})\n"
-        markdown_content += f"Query ID: {info['queryID']} - {info['queryName']}\n"
-        markdown_content += f"Description: {info['queryDescription']}\n"
-        markdown_content += f"Query Type: {info['queryType']}\n"
-        markdown_content += f"Query: ```text\n{info['query']}\n```\n\n"
+        markdown_content += f"{info['indent']}## DataSource: {info['dataSourceName']} (ID: {info['dataSourceID']})\n"
+        markdown_content += f"{info['indent']}Query ID: {info['queryID']} - {info['queryName']}\n"
+        markdown_content += f"{info['indent']}Description: {info['queryDescription']}\n"
+        markdown_content += f"{info['indent']}Query Type: {info['queryType']}\n"
+        markdown_content += f"{info['indent']}Query: ```text\n{info['indent']}{info['query']}\n{info['indent']}```\n\n"
 
     return markdown_content
 
