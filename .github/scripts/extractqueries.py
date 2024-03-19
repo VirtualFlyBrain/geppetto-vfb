7db33e56b5cf764858d9338cb9e36fd8dcea3b79
Rob Court <rcourt@ed.ac.uk>
2024-03-19 14:50:03 +0000

    Update extractqueries.py

--- .github/scripts/extractqueries.py
+++ .github/scripts/extractqueries.py
@@ -7,37 +7,43 @@ def parse_xmi(file_path):
         root = tree.getroot()
     return root
 
-def extract_simple_queries_with_data_source(root):
-    namespaces = {'xsi': 'http://www.w3.org/2001/XMLSchema-instance'}
-    simple_queries_info = []
-    
-    # Iterate through dataSources to find associated SimpleQuery instances
-    for data_source in root.findall('.//dataSources', namespaces):
-        ds_url = data_source.get('url', 'No URL provided')
-        # Find SimpleQuery instances within this dataSource
-        for query in data_source.findall('.//queries[@xsi:type="gep_2:SimpleQuery"]', namespaces):
+def extract_queries_with_data_source(root, namespaces):
+    data_sources = root.findall('.//dataSources', namespaces=namespaces)
+    queries_info = []
+
+    for ds in data_sources:
+        ds_id = ds.get('id')
+        ds_name = ds.get('name')
+        ds_url = ds.get('url', 'No URL provided')  # Assuming there's a URL attribute
+        # Process SimpleQuery instances within this dataSource
+        for query in ds.findall('.//queryChain[@xsi:type="gep_2:SimpleQuery"]', namespaces=namespaces):
             query_id = query.get('id')
-            query_string_encoded = query.get('queryString', '')
+            query_name = query.get('name')
+            query_description = query.get('description', '')
+            query_string_encoded = query.get('query', '')  # Correct attribute name for query string
             # Decode HTML entities in the query string
             query_string_decoded = html.unescape(query_string_encoded)
-            
-            simple_queries_info.append({
-                'id': query_id,
+
+            queries_info.append({
+                'dataSourceID': ds_id,
+                'dataSourceName': ds_name,
                 'dataSourceURL': ds_url,
+                'queryID': query_id,
+                'queryName': query_name,
+                'queryDescription': query_description,
                 'query': query_string_decoded
             })
-    
-    return simple_queries_info
 
-def generate_markdown_for_queries(simple_queries_info):
-    markdown_content = "# Simple Queries and Data Source URLs\n\n"
-    for info in simple_queries_info:
-        markdown_content += f"## Query ID: {info['id']}\n"
-        markdown_content += f"- DataSource URL: {info['dataSourceURL']}\n"
-        markdown_content += f"  Query: ```cypher\n{info['query']}\n```\n"
-        # Assuming the query can be directly appended to the data source URL
-        full_query_url = f"{info['dataSourceURL']}?query={html.escape(info['query'])}"
-        markdown_content += f"  Runnable Query URL: {full_query_url}\n\n"
+    return queries_info
+
+def generate_markdown_for_queries(queries_info):
+    markdown_content = "# Detailed Queries and Data Source Information\n\n"
+    for info in queries_info:
+        markdown_content += f"## Query ID: {info['queryID']} - {info['queryName']}\n"
+        markdown_content += f"Description: {info['queryDescription']}\n"
+        markdown_content += f"Data Source: {info['dataSourceName']} (ID: {info['dataSourceID']})\n"
+        markdown_content += f"Query: ```\n{info['query']}\n```\n\n"
+
     return markdown_content
 
 def save_to_file(content, file_path):
@@ -45,12 +51,13 @@ def save_to_file(content, file_path):
         file.write(content)
 
 def main(xmi_file_path, output_markdown_path):
+    namespaces = {'xsi': 'http://www.w3.org/2001/XMLSchema-instance'}
     root = parse_xmi(xmi_file_path)
-    simple_queries_info = extract_simple_queries_with_data_source(root)
-    markdown_content = generate_markdown_for_queries(simple_queries_info)
+    queries_info = extract_queries_with_data_source(root, namespaces)
+    markdown_content = generate_markdown_for_queries(queries_info)
     save_to_file(markdown_content, output_markdown_path)
 
 if __name__ == "__main__":
-    xmi_file_path = "./model/vfb.xmi"  # Adjust as necessary
+    xmi_file_path = "./model/vfb.xmi"  # Adjust the path as necessary
     output_markdown_path = "./model/query.md"  # The path where the markdown file will be saved
     main(xmi_file_path, output_markdown_path)
