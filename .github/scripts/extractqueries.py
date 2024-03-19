0af4a9f84637902c69c8c3d30c7768614dc5c974
Rob Court <rcourt@ed.ac.uk>
2024-03-19 18:06:37 +0000

    adding children

--- .github/scripts/extractqueries.py
+++ .github/scripts/extractqueries.py
@@ -39,9 +39,17 @@ def process_queries(element, indent, ds_id, ds_name, ds_url, queries_info, names
         else:
             queries_info.append(query_entry)
 
-        # Recursively process child queries of a compound query
+        # Specific handling for compound queries
         if query_type == "gep_2:CompoundQuery":
-            process_queries(query, indent + "    ", ds_id, ds_name, ds_url, queries_info, namespaces, query_name, query_entry)
+            query_entry['query'] = 'See child steps below'  # Update to indicate child steps will follow
+            child_queries = []  # Prepare to capture child queries
+            # Assume a function to extract and process child queries exists: process_child_queries(query, namespaces)
+            process_child_queries(query, namespaces, child_queries)
+            query_entry['childQueries'] = child_queries
+            if parent_info is not None:
+                parent_info['childQueries'].append(query_entry)
+            else:
+                queries_info.append(query_entry)
 
 def generate_markdown_for_all_queries(queries_info):
     markdown_content = "# Queries and Chains Across Data Sources\n\n"
@@ -53,10 +61,10 @@ def generate_markdown_for_all_queries(queries_info):
         markdown_content += f"{info['indent']}*Query*: ```\n{info['query']}\n```\n\n"
 
         # Process child queries if any
-        for child_query in info.get('childQueries', []):
-            markdown_content += f"{child_query['indent']}### {child_query['queryName']}\n"
-            markdown_content += f"{child_query['indent']}*Description*: {child_query['queryDescription']}\n"
-            markdown_content += f"{child_query['indent']}*Query*: ```\n{child_query['query']}\n```\n\n"
+        if 'childQueries' in info and info['childQueries']:
+            for child_query in info['childQueries']:
+                markdown_content += f"{child_query['indent']}### Child Query: {child_query['queryName']}\n"
+                markdown_content += f"{child_query['indent']}Description: {child_query['queryDescription']}\n"
 
     return markdown_content
 
