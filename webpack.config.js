037f028665aad31379c63c19772e394964c531ef
Rob Court <rcourt@ed.ac.uk>
2024-06-04 12:50:40 +0100

    Revert "Ensure ES6 is handled"
    This reverts commit 6c53d40b5f4530a15708e8cb80383e2ac749ddf7.

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,57 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const fs = require('fs');
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var fs = require('fs');
 
-let geppettoConfig;
+/*
+ *var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+ * <%=htmlWebpackPlugin.options.GEPPETTO_CONFIGURATION._webapp_folder%>
+ */
+var geppettoConfig;
 try {
   geppettoConfig = require('./GeppettoConfiguration.json');
   console.log('\nLoaded Geppetto config from file');
 } catch (e) {
-  console.error('\nFailed to load Geppetto Configuration');
+  // Failed to load config file
+  console.error('\nFailed to load Geppetto Configuration')
 }
+var geppetto_base_path = 'node_modules/@geppettoengine/geppetto-client'
+const geppetto_client_path = geppetto_base_path + '/geppetto-client';
 
-const geppetto_base_path = 'node_modules/@geppettoengine/geppetto-client';
-const geppetto_client_path = `${geppetto_base_path}/geppetto-client`;
+var publicPath = path.join("/", geppettoConfig.contextPath, "geppetto/build/");
+console.log("\nThe public path (used by the main bundle when including split bundles) is: " + publicPath);
 
-const publicPath = path.join("/", geppettoConfig.contextPath, "geppetto/build/");
-console.log(`\nThe public path (used by the main bundle when including split bundles) is: ${publicPath}`);
+var isProduction = process.argv.indexOf('-p') >= 0;
+console.log("\n Building for a " + ((isProduction) ? "production" : "development") + " environment")
 
-const isProduction = process.argv.indexOf('-p') >= 0;
-console.log(`\nBuilding for a ${(isProduction ? "production" : "development")} environment`);
+var availableExtensions = [];
 
-const availableExtensions = [
-  { from: path.resolve(__dirname, geppetto_client_path, "static/*"), to: 'static', flatten: true },
-  { from: path.resolve(__dirname, "static"), to: 'static' },
-  { from: 'model/*', to: './', flatten: true },
-  { from: 'images/*', to: './', flatten: true }
-];
+availableExtensions.push({ from: path.resolve(__dirname, geppetto_client_path, "static/*"), to: 'static', flatten: true });
+availableExtensions.push({ from: path.resolve(__dirname, "static"), to: 'static' });
+availableExtensions.push({ from: 'model/*', to: './', flatten: true });
+availableExtensions.push({ from: 'images/*', to: './', flatten: true });
 
 console.log(availableExtensions);
 
-module.exports = function (env) {
-  if (env !== undefined) {
+module.exports = function (env){
+  // geppettoConfig._webapp_folder
+  if (env != undefined){
     console.log(env);
-    if (env.contextPath) {
+    if (env.contextPath){
       geppettoConfig.contextPath = env.contextPath;
     }
-    if (env.useSsl) {
+    if (env.useSsl){
       geppettoConfig.useSsl = JSON.parse(env.useSsl);
     }
-    if (env.noTest) {
+    if (env.noTest){
       geppettoConfig.noTest = JSON.parse(env.noTest);
     }
-    if (env.embedded) {
+    if (env.embedded){
       geppettoConfig.embedded = JSON.parse(env.embedded);
     }
-    if (env.embedderURL) {
+    if (env.embedderURL){
       geppettoConfig.embedderURL = env.embedderURL;
     }
   }
@@ -54,12 +59,12 @@ module.exports = function (env) {
   console.log('Geppetto configuration \n');
   console.log(JSON.stringify(geppettoConfig, null, 2), '\n');
   
-  const entries = {
+  var entries = {
     main: path.resolve(__dirname, "Main.js"),
     admin: path.resolve(__dirname, geppetto_client_path, "js/pages/admin/admin.js"),
   };
 
-  console.log(`\nThe Webpack entries are:`);
+  console.log("\nThe Webpack entries are:");
   console.log(entries);
 
   return {
@@ -70,10 +75,10 @@ module.exports = function (env) {
         cacheGroups: {
           commons: {
             name: 'common',                   
-            minChunks: 2,
-            chunks: 'initial',
-            reuseExistingChunk: true,
-            enforce: true
+            minChunks: 2, // Minimum # of chunks which need to contain a module before it's moved into the commons chunk.
+            chunks: 'initial', // initial, async or all
+            reuseExistingChunk: true, // use existing chunk if available instead of creating new one
+            enforce: true // form this chunk irrespective of the size of the chunk
           }
         }
       }
@@ -85,17 +90,32 @@ module.exports = function (env) {
       publicPath: publicPath
     },
     plugins: [
+      /*
+       * new BundleAnalyzerPlugin({
+       *     analyzerMode: 'static'
+       * }),
+       */
       new CopyWebpackPlugin(availableExtensions),
       new HtmlWebpackPlugin({
         filename: 'geppetto.vm',
         template: path.resolve(__dirname, geppetto_client_path, 'js/pages/geppetto/geppetto.ejs'),
         GEPPETTO_CONFIGURATION: geppettoConfig,
+        /*
+         * chunks: ['main'] Not specifying the chunk since its not possible
+         * yet (need to go to Webpack2) to specify UTF-8 as charset without
+         * which we have errors
+         */
         chunks: []
       }),
       new HtmlWebpackPlugin({
         filename: 'admin.vm',
         template: path.resolve(__dirname, geppetto_client_path, 'js/pages/admin/admin.ejs'),
         GEPPETTO_CONFIGURATION: geppettoConfig,
+        /*
+         * chunks: ['admin'] Not specifying the chunk since its not possible
+         * yet (need to go to Webpack2) to specify UTF-8 as charset without
+         * which we have errors
+         */
         chunks: []
       }),
       new HtmlWebpackPlugin({
@@ -117,11 +137,11 @@ module.exports = function (env) {
     resolve: {
       alias: {
         root: path.resolve(__dirname),
-        '@geppettoengine/geppetto-client': path.resolve(__dirname, `${geppetto_client_path}/js`),
-        '@geppettoengine/geppetto-ui': path.resolve(__dirname, `${geppetto_base_path}/geppetto-ui/src`),
-        '@geppettoengine/geppetto-core': path.resolve(__dirname, `${geppetto_base_path}/geppetto-core/src`),
-        geppetto: path.resolve(__dirname, `${geppetto_client_path}/js/pages/geppetto/GEPPETTO.js`),
-        '@geppettoengine/geppetto-client-initialization': path.resolve(__dirname, `${geppetto_client_path}/js/pages/geppetto/main`),
+        '@geppettoengine/geppetto-client': path.resolve(__dirname, geppetto_client_path + '/js'),
+        '@geppettoengine/geppetto-ui': path.resolve(__dirname, geppetto_base_path + '/geppetto-ui/src'),
+        '@geppettoengine/geppetto-core': path.resolve(__dirname, geppetto_base_path + '/geppetto-core/src'),
+        geppetto: path.resolve(__dirname, geppetto_client_path, 'js/pages/geppetto/GEPPETTO.js'),
+        '@geppettoengine/geppetto-client-initialization': path.resolve(__dirname, geppetto_client_path, 'js/pages/geppetto/main'),
         handlebars: 'handlebars/dist/handlebars.js'
       },
       extensions: ['*', '.js', '.json', '.ts', '.tsx', '.jsx'],
@@ -131,14 +151,14 @@ module.exports = function (env) {
       rules: [
         {
           test: /\.(js|jsx)$/,
-          exclude: [/ami.min.js/, /node_modules\/(?!(@geppettoengine\/geppetto-client)\/).*/],
+          exclude: [/ami.min.js/, /node_modules\/(?!(@geppettoengine\/geppetto-client)\/).*/], 
           loader: 'babel-loader',
           query: { 
             presets: [['@babel/preset-env', { "modules": false }], '@babel/preset-react'],
-            plugins: ["@babel/plugin-transform-runtime", "@babel/plugin-proposal-class-properties"],
-            sourceType: "unambiguous"
+            plugins : ["@babel/plugin-transform-runtime", "@babel/plugin-proposal-class-properties"], sourceType: "unambiguous"
           }
         },
+        // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
         {
           test: /\.tsx?$/,
           loader: "awesome-typescript-loader"
@@ -164,7 +184,7 @@ module.exports = function (env) {
         },
         {
           test: /\.less$/,
-          loader: `style-loader!css-loader!less-loader?{"modifyVars":{"url":"${path.resolve(__dirname, geppettoConfig.themes)}"}}`
+          loader: 'style-loader!css-loader!less-loader?{"modifyVars":{"url":"\'' + path.resolve(__dirname, geppettoConfig.themes) + '\'"}}'
         },
         {
           test: /\.html$/,
@@ -182,5 +202,5 @@ module.exports = function (env) {
       child_process: 'empty',
       module: 'empty'
     }
-  };
+  }
 };
