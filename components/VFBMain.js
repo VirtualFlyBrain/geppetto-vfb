bb61d82765364afd89a988fd471fc1d768083fdf
Rob Court <robbie.court@gmail.com>
2025-04-21 15:19:36 +0100

    Revert "Fix instance type resolution in VFBMain to correctly retrieve type from window object"
    This reverts commit e53f63130d94461d6d1e83117a7ce7457e0da00d.

--- components/VFBMain.js
+++ components/VFBMain.js
@@ -371,7 +371,7 @@ class VFBMain extends React.Component {
     try { 
       instance = Instances.getInstance(path + "." + path + "_obj");
       if (!instance == undefined) {
-        insttype = window[path][path + '_obj'].getType();
+        insttype = instance.getType();
         if (!insttype == undefined && insttype.getType() == "ImportType") {
           insttype = instance.getType().resolve();
         }
