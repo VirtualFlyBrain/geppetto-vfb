e92a9fcfd1cd30ea9547fa178ecf8a8e2366d4be
Rob Court <robbie.court@gmail.com>
2025-04-25 16:18:11 +0100

    Add additional error handling for SWC instance retrieval in resolve3D method

--- components/VFBMain.js
+++ components/VFBMain.js
@@ -393,7 +393,13 @@ class VFBMain extends React.Component {
         }
       } catch (ignore) {
       }
+    } else {
+      try {
+        Instances.getInstance(path + "." + path + "_swc");
+      } catch (ignore) { 
+      }
     }
+
     // if no swc check if we have obj
     if (instance == undefined) {
       try {
