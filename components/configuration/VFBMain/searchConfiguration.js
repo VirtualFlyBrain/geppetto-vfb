a294e910dfbda50440813d33dbf9e528857cd8cb
Rob Court <robbie.court@gmail.com>
2026-03-19 21:06:00 +0000

    Update boost query weights and prioritize class terms in search results

--- components/configuration/VFBMain/searchConfiguration.js
+++ components/configuration/VFBMain/searchConfiguration.js
@@ -102,7 +102,7 @@ var datasourceConfiguration = {
       ],
       "rows": "150",
       "wt": "json",
-      "bq": "short_form:VFBexp*^10.0 short_form:VFB*^100.0 short_form:FBbt*^100.0 short_form:FBbt_00003982^2 facets_annotation:Deprecated^0.001 facets_annotation:DataSet^500.0 facets_annotation:pub^100.0",
+      "bq": "short_form:VFBexp*^10.0 short_form:VFB*^50.0 facets_annotation:Class^200.0 short_form:FBbt*^150.0 short_form:FBbt_00003982^2 facets_annotation:Deprecated^0.001 facets_annotation:DataSet^500.0 facets_annotation:pub^100.0",
     }
 };
 
@@ -340,6 +340,15 @@ var searchConfiguration = {
       return -1;
     }
     if (b.label.toLowerCase().indexOf(InputString.toLowerCase()) > -1 && b.label.toLowerCase().indexOf(InputString.toLowerCase()) < a.label.toLowerCase().indexOf(InputString.toLowerCase())) {
+      return 1;
+    }
+    // prioritise class terms (FBbt_) over individual terms (VFB_)
+    var aIsClass = a.id.indexOf("FBbt") > -1;
+    var bIsClass = b.id.indexOf("FBbt") > -1;
+    if (aIsClass && !bIsClass) {
+      return -1;
+    }
+    if (bIsClass && !aIsClass) {
       return 1;
     }
     // move up expression (VFBexp) terms
